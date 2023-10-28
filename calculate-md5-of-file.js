0a67d13ccdf3a5983de22935cabadf333dc56250
Stepan Boychuk <demonomiconnnn@gmail.com>
2023-10-28 11:21:12 +0300

    Redid without Promise

--- calculate-md5-of-file.js
+++ calculate-md5-of-file.js
@@ -1,15 +1,9 @@
 const crypto = require('crypto');
 const fs = require('fs');
 
-const calculateMD5Hash = (filePath) => {
-    return new Promise((resolve, reject) => {
-        const stream = fs.createReadStream(filePath);
-        const hash = crypto.createHash('md5');
-        hash.setEncoding('hex');
-        stream.pipe(hash);
-        stream.on('error', error => reject(error));
-        stream.on('end', () => resolve(hash.read()));
-    })
-}
+const stream = fs.createReadStream(process.argv[2]);
+const hash = crypto.createHash('md5').setEncoding('hex');
 
-calculateMD5Hash(process.argv[2])
+stream.pipe(hash);
+stream.on('error', error => console.error(error));
+stream.on('end', () => console.log(hash.read()));
